5ce1f5b0390c6c9a295db9d3b764b4f329bd7619
Geoff Cox <redgeoff@gmail.com>
2018-07-20 10:53:53 -0700

    fix(couchdb): 1 replica to avoid race conditions (#3)
    * fix(couchdb): 1 replica to avoid race conditions
    
    * fix(couchdb): 1 replica to avoid race conditions

--- create-couchdb-service.sh
+++ create-couchdb-service.sh
@@ -3,8 +3,11 @@
 # Change to script directory
 cd `dirname $0`
 
+# This value is set to 1 as otherwise you can experience race conditions when testing, e.g. when creating databases
+replicas=1
+
 # Create service
-docker service create --replicas 2 --name couchdb --network couchdb-network \
+docker service create --replicas $replicas --name couchdb --network couchdb-network \
   --hostname="couchdb{{.Task.Slot}}" \
   --mount type=bind,source=/home/ubuntu/common,destination=/common \
   --mount type=bind,source=/home/ubuntu/common/etc/local.ini,destination=/home/couchdb/couchdb/etc/local.d/local.ini \
